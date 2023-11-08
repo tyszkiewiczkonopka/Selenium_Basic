b2aedd9878811c47efd57bcf65fd23550ed24414
Magdalena <tyszkiewicz.konopqa@gmail.com>
2023-11-08 07:44:42 +0100

    Add private methods to form test class

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -2,19 +2,24 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 
 public class BaseTest {
     protected WebDriver driver;
     protected final String BASE_URL = "http://www.seleniumui.moderntester.pl/";
+    protected final String DEFAULT_DIRECTORY = "/Users/magdalena/IdeaProjects/files";
 
     @BeforeEach
     public void setup() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments(DEFAULT_DIRECTORY);
+
+        driver = new ChromeDriver(options);
     }
 
-//    @AfterEach
-//    public void quitdriver() {
-//        driver.quit();
-//    }
+    @AfterEach
+    public void quitdriver() {
+        driver.quit();
+    }
 }
--- src/test/java/FormTest.java
+++ src/test/java/FormTest.java
@@ -2,20 +2,79 @@ import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.time.Duration;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class FormTest extends BaseTest {
     private final Random random = new Random();
+    private Logger logger = LoggerFactory.getLogger("FormTest");
 
-
-    @Test
-    //@RepeatedTest(value = 10, name = RepeatedTest.LONG_DISPLAY_NAME)
+    @RepeatedTest(value = 10, name = RepeatedTest.LONG_DISPLAY_NAME)
     void should_successfully_sign_in() {
         driver.get(BASE_URL + "/form.php");
 
+        fillform();
+        chooseFileToUpload();
+        submitForm();
+
+        String validationSuccessMessage = "Form send with success";
+        String actualMessage = driver.findElement(By.id("validator-message")).getText();
+
+        assertThat(actualMessage)
+                .isEqualTo(validationSuccessMessage)
+                .withFailMessage("Validation message not as expected.");
+    }
+
+    @Test
+        //@RepeatedTest(value = 10, name = RepeatedTest.LONG_DISPLAY_NAME)
+    void should_find_downloaded_file_in_default_folder() throws InterruptedException {
+        driver.get(BASE_URL + "/form.php");
+        fillform();
+
+        File directory = new File(DEFAULT_DIRECTORY);
+        File[] files = directory.listFiles();
+        int numberOfFiles = 0;
+
+        if (files != null) {
+            numberOfFiles = files.length;
+            logger.info("Number of files in the directory: " + numberOfFiles);
+        } else {
+            logger.error("The directory " + DEFAULT_DIRECTORY + " is empty.");
+        }
+        driver.findElement(By.cssSelector(".btn.btn-secondary")).click();
+
+        Thread.sleep(2000);
+
+        String expectedFileName = "test-file-todownload.xlsx";
+
+
+//        assertThat(numberOfFiles)
+//                .withFailMessage("Number of files not as expected")
+//                .isEqualTo(numberOfFiles + 1);
+//
+//        boolean fileFound = Arrays.stream(directory.listFiles())
+//                .anyMatch(file -> file.getName().equals(expectedFileName));
+        File[] newFiles = directory.listFiles();
+
+
+        assertThat(newFiles)
+                //.withFailMessage("File not found in the directory: "+ DEFAULT_DIRECTORY)
+                .extracting(File::getName) // Extract the names of the files
+                .contains(expectedFileName);
+    }
+
+    private void fillform() {
         driver.findElement(By.id("inputFirstName3")).sendKeys("Magdalena");
         driver.findElement(By.id("inputLastName3")).sendKeys("Tyszkiewicz");
         driver.findElement(By.id("inputEmail3")).sendKeys("mtyszkiewicz@sii.pl");
@@ -35,7 +94,23 @@ public class FormTest extends BaseTest {
         WebElement selectContinentsElement = driver.findElement(By.id("selectContinents"));
         Select selectContinent = new Select(selectContinentsElement);
         List<WebElement> continents = selectContinent.getOptions();
-        selectContinent.selectByIndex(random.nextInt(continents.size() -1) +1);
+        selectContinent.selectByIndex(random.nextInt(continents.size() - 1) + 1);
 
+        WebElement selectCommandsElement = driver.findElement(By.id("selectSeleniumCommands"));
+        Select selectCommand = new Select(selectCommandsElement);
+        selectCommand.selectByValue("switch-commands");
+        selectCommand.selectByValue("wait-commands");
     }
+
+    private void chooseFileToUpload() {
+        String uploadedFileDirectory = "/Users/magdalena/Documents/GRAFIKA/Ikony/PNG/basen.png";
+        driver.findElement(By.id("chooseFile")).sendKeys(uploadedFileDirectory);
+    }
+
+    private void submitForm() {
+        driver.findElement(By.cssSelector(".btn.btn-primary")).click();
+    }
+
+
 }
+
